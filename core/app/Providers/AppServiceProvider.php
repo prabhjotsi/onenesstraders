7029c23b83b2ac4eaf1f666b46bd160f7a57d738
prabhjot <prabhjots825@gmail.com>
2023-03-07 21:28:05 +0530

    updated1

--- core/app/Providers/AppServiceProvider.php
+++ core/app/Providers/AppServiceProvider.php
@@ -29,50 +29,50 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
-    {
+    // public function boot()
+    // {
 
-        $activeTemplate = activeTemplate();
-        $general        = gs();
+    //     $activeTemplate = activeTemplate();
+    //     $general        = gs();
 
-        $viewShare['general']            = $general;
-        $viewShare['activeTemplate']     = $activeTemplate;
-        $viewShare['activeTemplateTrue'] = activeTemplate(true);
-        $viewShare['language']           = Language::all();
-        $viewShare['emptyMessage']       = 'Data not found';
-        view()->share($viewShare);
+    //     $viewShare['general']            = $general;
+    //     $viewShare['activeTemplate']     = $activeTemplate;
+    //     $viewShare['activeTemplateTrue'] = activeTemplate(true);
+    //     $viewShare['language']           = Language::all();
+    //     $viewShare['emptyMessage']       = 'Data not found';
+    //     view()->share($viewShare);
 
-        view()->composer('admin.partials.sidenav', function ($view) {
-            $view->with([
-                'bannedUsersCount'           => User::banned()->count(),
-                'emailUnverifiedUsersCount'  => User::emailUnverified()->count(),
-                'mobileUnverifiedUsersCount' => User::mobileUnverified()->count(),
-                'kycUnverifiedUsersCount'    => User::kycUnverified()->count(),
-                'kycPendingUsersCount'       => User::kycPending()->count(),
-                'pendingTicketCount'         => SupportTicket::whereIN('status', [0, 2])->count(),
-                'pendingDepositsCount'       => Deposit::pending()->count(),
-                'pendingWithdrawCount'       => Withdrawal::pending()->count(),
-            ]);
-        });
+    //     view()->composer('admin.partials.sidenav', function ($view) {
+    //         $view->with([
+    //             'bannedUsersCount'           => User::banned()->count(),
+    //             'emailUnverifiedUsersCount'  => User::emailUnverified()->count(),
+    //             'mobileUnverifiedUsersCount' => User::mobileUnverified()->count(),
+    //             'kycUnverifiedUsersCount'    => User::kycUnverified()->count(),
+    //             'kycPendingUsersCount'       => User::kycPending()->count(),
+    //             'pendingTicketCount'         => SupportTicket::whereIN('status', [0, 2])->count(),
+    //             'pendingDepositsCount'       => Deposit::pending()->count(),
+    //             'pendingWithdrawCount'       => Withdrawal::pending()->count(),
+    //         ]);
+    //     });
 
-        view()->composer('admin.partials.topnav', function ($view) {
-            $view->with([
-                'adminNotifications'     => AdminNotification::where('is_read', 0)->with('user')->orderBy('id', 'desc')->take(10)->get(),
-                'adminNotificationCount' => AdminNotification::where('is_read', 0)->count(),
-            ]);
-        });
+    //     view()->composer('admin.partials.topnav', function ($view) {
+    //         $view->with([
+    //             'adminNotifications'     => AdminNotification::where('is_read', 0)->with('user')->orderBy('id', 'desc')->take(10)->get(),
+    //             'adminNotificationCount' => AdminNotification::where('is_read', 0)->count(),
+    //         ]);
+    //     });
 
-        view()->composer('partials.seo', function ($view) {
-            $seo = Frontend::where('data_keys', 'seo.data')->first();
-            $view->with([
-                'seo' => $seo ? $seo->data_values : $seo,
-            ]);
-        });
+    //     view()->composer('partials.seo', function ($view) {
+    //         $seo = Frontend::where('data_keys', 'seo.data')->first();
+    //         $view->with([
+    //             'seo' => $seo ? $seo->data_values : $seo,
+    //         ]);
+    //     });
 
-        if ($general->force_ssl) {
-            \URL::forceScheme('https');
-        }
+    //     if ($general->force_ssl) {
+    //         \URL::forceScheme('https');
+    //     }
 
-        Paginator::useBootstrapFour();
-    }
+    //     Paginator::useBootstrapFour();
+    // }
 }
